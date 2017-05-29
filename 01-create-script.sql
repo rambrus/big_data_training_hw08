cccd73addadc7078a0ef43f21619d7ad9e49b866
Robert Ambrus <v-rambrus@expedia.com>
2017-05-29 16:43:27 +0200

    @noissue: initial commit

---
+++ 01-create-script.sql
@@ -0,0 +1,27 @@
+-- Fix the following Cassandra DDL, so that all queries from the files below can be executed without changes
+ -- 02-upsert-readonly.sql and
+ -- 03-query-readonly.sql
+
+-- Create keyspace (press TAB for autocomplete in cqlsh)
+CREATE KEYSPACE homework WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3};
+
+-- Use keyspace
+USE homework;
+
+-- Fix the following table creation script so that the subsequent queries work as expected
+-- The table contains both the invoice header and the invoice detail fields
+CREATE table invoice
+(
+  -- header fields
+  invoice_id text,
+  invoice_date date static,
+  invoice_address text static,
+  -- detail fields
+  line_id int,
+  article_name text,
+  article_price decimal,
+  PRIMARY KEY (invoice_id, article_name)
+);
+-- WITH CLUSTERING ORDER BY (line_id ASC, article_name ASC);
+
+CREATE INDEX ON invoice(invoice_date);
